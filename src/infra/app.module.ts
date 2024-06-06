c7a1b6b86089d0d5d3d97025e66cd55d6b7c1ebb
Iuri Reis <123806396+ithauront@users.noreply.github.com>
2024-06-06 13:42:42 +0200

    start to define the env

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common'
 import { PrismaService } from './prisma/prisma.service'
 import { CreateCourierController } from './controllers/create-courier.controller'
+import { ConfigModule } from '@nestjs/config'
+import { envSchema } from './env'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+  ],
   controllers: [CreateCourierController,],
   providers: [PrismaService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './infra/app.module'
+import { ConfigService } from '@nestjs/config'
+import { Env } from './infra/env'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { logger: false })
-  await app.listen(3353)
+  const configService = app.get<ConfigService<Env, true>>(ConfigService)
+  const port = configService.get('PORT', { infer: true })
+  await app.listen(port)
 }
 bootstrap()
